b83cc088761d36a65fbac7da10b78acc9df687fa
eceeb <dev.mail@icloud.com>
2015-02-03 18:33:31 +0100

    Refactoring

--- src/main/java/de/eliyo/utils/WebsiteLoader.java
+++ src/main/java/de/eliyo/utils/WebsiteLoader.java
@@ -1,6 +1,7 @@
 package de.eliyo.utils;
 
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -11,29 +12,41 @@ import org.apache.commons.io.IOUtils;
 @Singleton
 public class WebsiteLoader {
 
-	private String websiteUrl;
 	private String body;
-	private boolean previouslyLoaded;
+	private String websiteUrl;
 
 	public WebsiteLoader load(String websiteUrl) {
-		if (websiteUrl.equals(this.websiteUrl))
-			previouslyLoaded = true;
-		else
-			previouslyLoaded = false;
+
+		if (previouslyLoaded(websiteUrl)) {
+			System.out.println(websiteUrl + " was prev loaded...");
+			return this;
+		}
 		this.websiteUrl = websiteUrl;
+
+		body = loadBody();
+
 		return this;
 	}
 
-	public String getBody() throws Exception {
-		if(previouslyLoaded)
-			return body;
-			
-		URL url = new URL(websiteUrl);
-		URLConnection con = url.openConnection();
-		InputStream in = con.getInputStream();
-		String encoding = con.getContentEncoding();
-		encoding = encoding == null ? "UTF-8" : encoding;
-		body = IOUtils.toString(in, encoding);
+	private String loadBody() {
+		try {
+			URL url = new URL(websiteUrl);
+			URLConnection con = url.openConnection();
+			InputStream in = con.getInputStream();
+			String encoding = con.getContentEncoding();
+			encoding = encoding == null ? "UTF-8" : encoding;
+			body = IOUtils.toString(in, encoding);
+		} catch (Exception x) {
+			return "";
+		}
+		return body;
+	}
+
+	private boolean previouslyLoaded(String websiteUrl) {
+		return websiteUrl.equals(this.websiteUrl);
+	}
+
+	public String getBody() {
 		return body;
 	}
 }
