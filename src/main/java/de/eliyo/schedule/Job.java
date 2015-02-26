c35d024c583c934777f42094c69626ccc62a0cde
eceeb <dev.mail@icloud.com>
2015-02-26 23:38:55 +0100

    Make scheduler stateless

--- src/main/java/de/eliyo/schedule/Job.java
+++ src/main/java/de/eliyo/schedule/Job.java
@@ -7,7 +7,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.ejb.Schedule;
-import javax.ejb.Singleton;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import de.eliyo.entity.Wanted;
@@ -16,7 +16,7 @@ import de.eliyo.utils.WebsiteLoader;
 import de.eliyo.utils.MailSender;
 import de.eliyo.utils.Crawler;
 
-@Singleton
+@Stateless
 public class Job {
 
 	@Inject Crawler search;
