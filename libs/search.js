7fa47fc5edf300a7eb67a25cd5505378ffce923a
eceeb <dev.mail@icloud.com>
2015-11-17 18:48:18 +0100

    Refactor

--- libs/search.js
+++ libs/search.js
@@ -2,15 +2,10 @@ var mail    = require('./mail')
 var emitter = require('./emitter')
 
 
-module.exports = function() {
+module.exports = function () {
 
 	function markAsFound (row) {
 		emitter.emit('foundRow', row)
-	}
-
-	function log(row, found) {
-		var txt = found ? 'Found something for: ' : 'Nothing found for: '
-		console.log(txt + row.seek)
 	}
 
 	return {
@@ -18,15 +13,15 @@ module.exports = function() {
 		after : function (row, body) {
 
 			var reg = new RegExp(row.seek, "i")
+
 			if (~body.search(reg)) {
-				log(row, true)
 				row.found = true
 				mail.send(row)
 				markAsFound(row)
 			}
-			else {
-				log(row, false)
-			}
+
+			var txt = row.found ? 'Found something for: ' : 'Nothing found for: '
+			console.log(txt + row.seek)
 		},
 	}
 }()
