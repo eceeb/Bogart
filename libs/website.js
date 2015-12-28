63c278565cbf05d302b4ff7812f2928df124122b
eceeb <dev.mail@icloud.com>
2015-12-28 19:45:57 +0100

    Single request for multiple searches on same site

--- libs/website.js
+++ libs/website.js
@@ -4,17 +4,28 @@ var searchG2a = require('./searchG2a')
 
 module.exports = function() {
 
+	function searchWebsiteForEntries (searches, url) {
+
+		request(url, function (error, response, body) {
+			for(var i in searches) {
+				if (!error && response.statusCode == 200) {
+					if (~url.search('www.g2a.com'))
+						searchG2a.after(searches[i], body)
+					else
+						search.after(searches[i], body)
+				}
+			}
+		})
+
+	}
+
 	return {
 
-		load : function(row) {
-			request(row.url, function (error, response, body) {
-				if (!error && response.statusCode == 200) {
-					if (row.url.lastIndexOf('https://www.g2a.com', 0) === 0)
-						searchG2a.after(row, body)
-					else
-						search.after(row, body)
-				}
-			})
+		load : function(urlToSearches) {
+
+			urlToSearches.forEach( function(values, key) {
+				searchWebsiteForEntries(values, key)
+			}, urlToSearches)
 		},
 	}
 }()
