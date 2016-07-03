5ad108222446ece1065f1bb45c91f9981944d86f
eceeb <dev.mail@icloud.com>
2016-07-03 22:40:28 +0200

    Shorten URLs in mailings

--- libs/mail.js
+++ libs/mail.js
@@ -1,3 +1,4 @@
+var shorturl    = require('shorturl-2');
 var nodemailer  = require('nodemailer')
 var transporter = nodemailer.createTransport(process.env.EMAIL_URI)
 
@@ -7,19 +8,22 @@ var mail = function () {
     return {
 
         send : function (row) {
-           
-            var mailOptions = {
-                to      : row.email, 
-                text    : 'Checkout ' + row.url,
-                subject : 'Hurray found something...',
-                from    : '"Humphrey Notification" <humphrey.notify@hotmail.com>'
-            }
 
-            transporter.sendMail(mailOptions, function(error, info) {
-                if (error)
-                    console.log('Fatal mailing error: ' + error)
-                else
-                    console.log(info)
+            shorturl(row.url, function(result) {
+
+                var mailOptions = {
+                    to      : row.email,
+                    text    : 'Checkout ' + result || row.url,
+                    subject : 'Hurray found something',
+                    from    : '"Humphrey Notification" <humphrey.notify@hotmail.com>'
+                }
+
+                 transporter.sendMail(mailOptions, function(error, info) {
+                    if (error)
+                        console.log('Fatal mailing error: ' + error)
+                    else
+                        console.log(info)
+                })
             })
         }
     }
